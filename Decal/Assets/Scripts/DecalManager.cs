d5ec6a0cd225f9894eca233ec3bc58014904698b
SmailZee <alan.smail@futuregames.nu>
2019-05-02 11:08:46 +0200

    First commit

---
+++ Decal/Assets/Scripts/DecalManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecalManager : MonoBehaviour
+{
+    public GameObject DecalPrefab;
+    int shootingPoolFreeIndex = 0;
+
+    GameObject[] shootingPool = new GameObject[10];
+    Queue<GameObject> queueForDecals = new Queue<GameObject>();
+
+    #region Singleton
+    public static DecalManager instance;
+    private void Awake()
+    {
+        if (instance != null)
+            Destroy(instance);
+        instance = this;
+    }
+    #endregion
+
+    void Start()
+    {
+       
+            for (int i = 0; i < shootingPool.Length; i++)
+            {
+                shootingPool[i] = Instantiate(DecalPrefab, transform.position, Quaternion.Euler(Vector3.zero), this.gameObject.transform);
+            shootingPool[i].gameObject.SetActive(false);
+
+            }
+        
+    }
+    public GameObject GetInactiveDecal()
+    {
+        for (int i = 0; i < shootingPool.Length; i++)
+        {
+            if(shootingPool[i].activeInHierarchy == false)
+            {
+                queueForDecals.Enqueue(shootingPool[i]);
+                return shootingPool[i];
+            }
+           
+        }
+        GameObject tempDecals = queueForDecals.Dequeue();
+        queueForDecals.Enqueue(tempDecals);
+        return tempDecals;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Decal/Assets/Scripts/Shoot.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    
+    DecalManager DecalM;
+    GameObject tempDecal;
+
+    public GameObject DecalHandler;
+    //if(Input.GetKey("RightArrow"))
+    //    {
+    //        transform.location = Transform.location + 1
+    //    }
+    private void Start()
+    {
+        DecalM = DecalHandler.GetComponent<DecalManager>();
+    }
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Fire();
+            Debug.Log("FIRE");
+
+        }
+
+    }
+    private void Fire()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray, out hitInfo, 500f))
+        {
+            tempDecal = DecalM.GetInactiveDecal();
+            tempDecal.SetActive(true);
+            tempDecal.transform.position = hitInfo.point + ((hitInfo.normal)* 0.01f);
+            tempDecal.transform.rotation = Quaternion.FromToRotation(Vector3.back, hitInfo.normal);
+            //Instantiate(DecalPrefab, hitInfo.point + (hitInfo.normal * 0.01f), Quaternion.FromToRotation(Vector3.back, hitInfo.normal));
+        }
+
+
+    }
+
+    
+
+
+
+
+}
